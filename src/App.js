6a908fe80940b6a46426860a58595352906fae84
koji9412 <koji0412ohta@gmail.com>
2022-08-03 10:07:01 +0900

    add

--- src/App.js
+++ src/App.js
@@ -11,15 +11,33 @@ import './App.css';
 
 function App() {
   const [city, setCity] = useState("");
+  const [results, setResults] = useState({
+    country: "",
+    cityName: "",
+    temperature: "",
+    conditionText: "",
+    icon: "",
+  });
   const getWeather = (e) => {
       e.preventDefault();
-      axios.get("https://api.weatherapi.com/v1/current.json?key="+process.env.REACT_APP_WEATHER_API_KEY+"&q=London&aqi=no").then(res => console.log(res))
+      axios.get(`https://api.weatherapi.com/v1/current.json?key=`+process.env.REACT_APP_WEATHER_API_KEY+`&q=${city}&aqi=no`)
+        .then(res => {
+          setResults({
+            country: res.data.location.country,
+            cityName: res.data.location.name,
+            temperature: res.data.current.temp_c,
+            conditionText: res.data.current.condition.text,
+            icon: res.data.current.condition.icon,
+          })
+      })
   }
   return (
-    <div className="test">
-      <Title />
-      <Form setCity={setCity} getWeather={getWeather}/>
-      <Results />
+    <div className="wrapper">
+      <div className="container">
+          <Title />
+          <Form setCity={setCity} getWeather={getWeather}/>
+          <Results results={results}/>
+      </div>
     </div>
   );
 }
